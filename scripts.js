8e7b3d2c712a49f7db6d02e3b75dab43640052fd
Pedro Gonzalez <pedrogcernuda@gmail.com>
2022-04-02 15:21:00 -0600

    Trabajo final

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-function calcular(){     
+function calcular() {
     var consumo = parseFloat(document.getElementById("consumo").value);
-    var factor = parseFloat(document.getElementById("factor").value);   
-    var res= consumo*factor; 
-    console.log(res);
-    document.getElementById("resultado").value = res;
-};
+    var factor = parseFloat(document.getElementById("factor").value);
+    var res = consumo * factor;
+    document.getElementById("resultado").value = res + " kg CO2e";
+  }
